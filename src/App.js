3bb48e3be4215bfce83dc16b7158054d0f8ead3d
Mikaela22 <61974476+Mikaela22@users.noreply.github.com>
2020-11-11 11:44:28 -0300

    error in component home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./components/Home/Home";
+import Home from "./components/home/Home";
 import "./App.css";
 
 function App() {
