3e724c9d39d440cbcdb149154300d52a0ff2b803
Andrés Menchaca <andresfullstack@gmail.com>
2020-11-11 15:32:29 -0300

    Revert "update"
    This reverts commit 9b205635fa5d65ae9bf934e23d4de17a0d22e526.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import carritoActions from "../../redux/Actions/carritoActions";
-import { useDispatch } from "react-redux";
+import {Link} from "react-router-dom";
+import carritoActions from "../../../redux/Actions/carritoActions"
+import {useDispatch} from "react-redux"
 
-export default function Card({ producto }) {
-  const dispatch = useDispatch();
+export default function Card ({producto}) {
+    const dispatch = useDispatch();
 
-  function handleAddItem(event, item) {
-    event.preventDefault();
-    dispatch(carritoActions.addItem(item));
-  }
 
-  return (
-    <div class="col mb-4">
-      <div class="card">
-        <img src={producto.image} alt="product" />
-        <div class="card-body d-flex justify-content-center">
-          <div>
-            <h5 class="card-title">{producto.name}</h5>
-            <Link to={`/producto/${producto.slug}`} className="link-producto">
-              {producto.description}
-            </Link>
+    function handleAddItem(event, item) {
+        event.preventDefault();
+        dispatch(carritoActions.addItem(item));
+    }
+
+    return(
+        <div class="col mb-4">
+            <div class="card">
+              <img src={producto.image} alt="product" />
+              <div class="card-body d-flex justify-content-center">
+                <div>
+                  <h5 class="card-title">{producto.name}</h5>
+                  <Link to={`/producto/${producto.slug}`} className="link-producto">
+                    {producto.description}
+                  </Link>
 
             <h4>{producto.price}</h4>
 
