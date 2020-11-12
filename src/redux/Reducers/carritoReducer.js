77bfb70a9ae8110cb32dc315e9c3efbcdecbc065
Nacho A <48703982+MateSeco@users.noreply.github.com>
2020-11-12 10:32:28 -0300

    cuenta de carrito

--- src/redux/Reducers/carritoReducer.js
+++ src/redux/Reducers/carritoReducer.js
@@ -1,10 +1,14 @@
 export default function carritoReducer(
-  state = { user: [], items: [], estado: "" },
+  state = { user: [], items: [], cantIntems: 0, estado: "" },
   action
 ) {
   switch (action.type) {
     case "ADD_ITEM":
-      return { ...state, items: [...state.items, action.payload] };
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+        cantItems: [...state.items, action.payload].length,
+      };
     default:
       return state;
   }
