064204ee6e08dbe16ace7d0499fc10e5a330f658
Andrés Menchaca <andresfullstack@gmail.com>
2020-11-18 10:59:04 -0300

    Nav styles edit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -32,54 +32,52 @@ export default function NavBar() {
   return (
     <Navbar className="nav-bar" variant="white" expand="lg">
       <div className="container navBar-contenedor">
-        <div>
-          <Navbar.Brand as={Link} to="/" className="brand">
-            Electro Hack <i className="fas fa-desktop"></i>
-          </Navbar.Brand>
-        </div>
+        <Navbar.Brand as={Link} to="/" className="brand">
+          Electro Hack <i className="fas fa-desktop"></i>
+        </Navbar.Brand>
 
-        <div>
-          <Navbar.Collapse>
-            <Nav className="mr-auto ">
-              {categories.map((category) => {
-                return (
-                  <Nav.Link
-                    key={category._id}
-                    as={Link}
-                    to={`/categoria/${category.slug}`}
-                    id="links"
-                  >
-                    <p>{category.type}</p>
-                  </Nav.Link>
-                );
-              })}
-
-              {!user.token && (
-                <Nav.Link as={Link} to="/registro">
-                  <i className="fas fa-user-circle login-register-cart">
-                    {" "}
-                    Ingresar
-                  </i>
-                </Nav.Link>
-              )}
-
-              {user.token && (
-                <Nav.Link as={Link} to="/login" onClick={handleClick}>
-                  <i className="fas fa-user-circle login-register-cart">
-                    Salir
-                  </i>
-                </Nav.Link>
-              )}
-
-              <Nav.Link as={Link} to="/pedidos">
-                <i className="fas fa-shopping-cart login-register-cart">
-                  <span className="cartCount">{items && totalItems}</span>
-                </i>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
         <Navbar.Toggle id="burger-button" />
+
+        <Navbar.Collapse>
+          <Nav className="ml-auto pl-3 ">
+            {categories.map((category) => {
+              return (
+                <Nav.Link
+                  key={category._id}
+                  as={Link}
+                  to={`/categoria/${category.slug}`}
+                  id="links"
+                >
+                  <p>{category.type}</p>
+                </Nav.Link>
+              );
+            })}
+
+            {!user.token && (
+              <Nav.Link as={Link} to="/registro" id="links">
+                <p className="login-registro">
+                  <i className="fas fa-user-circle login-register-cart"></i>
+                  Ingresar
+                </p>
+              </Nav.Link>
+            )}
+
+            {user.token && (
+              <Nav.Link as={Link} to="/login" onClick={handleClick} id="links">
+                <p className="login-registro">
+                  <i className="fas fa-user-circle login-register-cart"></i>
+                  Salir
+                </p>
+              </Nav.Link>
+            )}
+
+            <Nav.Link as={Link} to="/pedidos" id="links">
+              <i className="fas fa-shopping-cart login-register-cart">
+                <span className="cartCount">{items && totalItems}</span>
+              </i>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </div>
     </Navbar>
   );
