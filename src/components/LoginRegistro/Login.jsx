510a03f9eb8acae4b72d09580597df67c7613638
Mikaela22 <61974476+Mikaela22@users.noreply.github.com>
2020-11-19 10:08:50 -0300

    changes

--- src/components/LoginRegistro/Login.jsx
+++ src/components/LoginRegistro/Login.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-// import "./loginRegistro.css";
+import "./loginRegistro.css";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import axiosCall from "../../utils/axiosCall";
 import { actionCreators } from "../../redux/Actions/userActions";
+
 export default function Login() {
   const dispatch = useDispatch();
   const history = useHistory();
@@ -26,61 +27,59 @@ export default function Login() {
     });
   }
   return (
-    <>
+    <div className="main">
       <NavBar />
-      <div className="main">
-        <section className="signin">
-          <div className="containerLogin">
-            <div className="signup-content">
-              <form
-                id="signup-form"
-                className="signup-form"
-                onSubmit={(e) => {
-                  handleLogin(e);
-                }}
-              >
-                <h2 className="form-title">Ingresa a tu cuenta</h2>
+      <section className="signup">
+        <div className="containerLogin">
+          <div className="signup-content">
+            <form
+              id="signup-form"
+              className="signup-form"
+              onSubmit={(e) => {
+                handleLogin(e);
+              }}
+            >
+              <h2 className="form-title">Ingresa a tu cuenta</h2>
 
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className="form-input"
-                    name="email"
-                    id="email"
-                    placeholder="Email"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className="form-input"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <span
-                    toggle="#password"
-                    className="zmdi zmdi-eye field-icon toggle-password"
-                  ></span>
-                </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-input"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-input"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <span
+                  toggle="#password"
+                  className="zmdi zmdi-eye field-icon toggle-password"
+                ></span>
+              </div>
 
-                <div className="form-group">
-                  <input
-                    type="submit"
-                    name="submit"
-                    id="submit"
-                    className="form-submit"
-                    value="Iniciar sesión"
-                  />
-                </div>
-              </form>
-            </div>
+              <div className="form-group">
+                <input
+                  type="submit"
+                  name="submit"
+                  id="submit"
+                  className="form-submit"
+                  value="Iniciar sesión"
+                />
+              </div>
+            </form>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
       <Footer />
-    </>
+    </div>
   );
 }
