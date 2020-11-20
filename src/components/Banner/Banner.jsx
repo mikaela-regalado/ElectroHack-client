eaa28b5efd59be605fc394e518c6cf4ce2f5e7b9
Mikaela22 <61974476+Mikaela22@users.noreply.github.com>
2020-11-20 12:12:11 -0300

    new images for carousel

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,76 @@
 import React from "react";
 import image from "../../img/climatizacion1.jpg";
-import "./Banner.css";
+import { Link } from "react-router-dom";
+/* import "./Banner.css"; */
 
 export default function Banner() {
   return (
-    <div className="banner">
-      <img src={image} alt="banner" />
+    /*  <div
+      id="carouselExampleFade"
+      className="carousel slide carousel-fade"
+      data-ride="carousel"
+    >
+      <div className="carousel-inner">
+        <div className="carousel-item">
+          <img src={image} className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <a
+        class="carousel-control-prev"
+        href="#carouselExampleFade"
+        role="button"
+        data-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#carouselExampleFade"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div> */
+    <div
+      id="carouselExampleFade"
+      class="carousel slide carousel-fade"
+      data-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="..." class="d-block w-100" alt="..."></img>
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="..."></img>
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="..."></img>
+        </div>
+      </div>
+      <Link
+        class="carousel-control-prev"
+        href="#carouselExampleFade"
+        role="button"
+        data-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </Link>
+      <Link
+        class="carousel-control-next"
+        href="#carouselExampleFade"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </Link>
     </div>
   );
 }
